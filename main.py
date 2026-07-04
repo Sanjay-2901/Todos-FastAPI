95dc8d57a9c901ed15ea57e6828491278610d8d8
Sanjay-2901 <sanjay.k@yavar.ai>
2026-07-04 22:23:48 +0530

    Feat: Users router

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 import models
-from routers import admin, auth, todos
+from routers import admin, auth, todos, users
 from database import engine
 
 app = FastAPI()
@@ -9,6 +9,7 @@ app = FastAPI()
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(users.router)
 
 
 models.Base.metadata.create_all(bind=engine)
